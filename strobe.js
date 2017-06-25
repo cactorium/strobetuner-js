28ce41ba445258c4d91ea95928fa90a5a46e2bec
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-06-25 05:43:57 -0400

    Much closer to actually working

--- strobe.js
+++ strobe.js
@@ -1,30 +1,33 @@
-
+'use strict'
 // TODO
 
+var t = 0.5
 // TODO
-function Strobe(audioCtx, glCtx) {
+function StrobeTuner(audioCtx, glCtx) {
   var me = this
   var log = function(str) {
     console.log(str)
   }
 
-  this.buffer = new Float32Array(Strobe.BUF_SZ)
+  // Audio related stuff
+  this.buffer = new Float32Array(StrobeTuner.BUF_SZ)
   this.bufferLen = 0
   this.sampleRate = 0
-  this.offset = 0.0
+  this.offset = 1.0
+  this.baseFrequency = 440.0
 
   this.newData = false
   this.audioNode = audioCtx.createScriptProcessor()
   this.audioNode.onaudioprocess = function(e) {
     var buf = e.inputBuffer
     log('got data')
-    log(buf)
+    // log(buf)
 
     me.sampleRate = buf.sampleRate
     if (buf.length + me.bufferLen > me.buffer.length) {
       log('resizing buffer')
       // resize the buffer
-      if (me.buffer.length > Strobe.MAX_BUF_SZ) {
+      if (me.buffer.length > StrobeTuner.MAX_BUF_SZ) {
         log('audio overflow!')
         return
       }
@@ -61,17 +64,180 @@ function Strobe(audioCtx, glCtx) {
     this.newData = false
   }
 
-  var draw = null
 
-  var initOnePoly = function() {
-    log('init one poly')
-    draw = function() {
+  // WebGL related stuff
+  var setupShader = function(source, type) {
+    var shader = glCtx.createShader(type)
+    glCtx.shaderSource(shader, source)
+    glCtx.compileShader(shader)
+    if (!glCtx.getShaderParameter(shader, glCtx.COMPILE_STATUS)) {
+      log('shader compilation error: ' + glCtx.getShaderInfoLog(shader))
+      glCtx.deleteShader(shader)
+      shader = null
     }
+
+    return shader
+  }
+  var setupProgram = function(vShader, fShader) {
+    var program = glCtx.createProgram()
+    glCtx.attachShader(program, vShader)
+    glCtx.attachShader(program, fShader)
+    glCtx.linkProgram(program)
+    if (!glCtx.getProgramParameter(program, glCtx.LINK_STATUS)) {
+      log('program compilation error: ' + glCtx.getProgramInfoLog(program))
+      glCtx.deleteProgram(program)
+      program = null
+    }
+
+    return program
   }
 
+  var draw = null
+  var clearGlState = null
+
+  this.brightGain = 1.00
+  this.brightOffset = 0.00
+
   var initManyPoly = function() {
-    log('init many poly')
+    var inited = false
+
+    // set up the initial OpenGL state
+    glCtx.clearColor(0.0, 0.0, 0.0, 1.0) // start off in complete darkness
+    glCtx.disable(glCtx.DEPTH_TEST) // draw all polygons
+    glCtx.enable(glCtx.BLEND) // turn on blending
+    glCtx.blendFunc(glCtx.ONE, glCtx.ONE) // accumulate values
+
+    var vertexSource = [
+      "attribute mediump vec4 aVertexPosition;",
+      "attribute mediump vec3 aStrobeCoord;",
+      "",
+      "varying mediump vec3 strobeCoord;",
+      "",
+      "void main(void) {",
+      "  gl_Position = aVertexPosition;",
+      "  strobeCoord = aStrobeCoord;",
+      "}"
+    ].join("\n")
+    var fragmentSource = [
+      "const lowp float numMultipliers = 8.0;",
+      "",
+      "uniform mediump float gain;",
+      "uniform mediump float offset;",
+      "",
+      "varying mediump vec3 strobeCoord;",
+      "",
+      "void main(void) {",
+      "  lowp float multiplier = 1.0 + floor(numMultipliers*strobeCoord.x);",
+      "  mediump float pos = fract(multiplier*strobeCoord.y);",
+      "  lowp float v = gain * strobeCoord.z;",
+      "  if (pos > 0.5) {",
+      "    v = offset + v;",
+      "  } else {",
+      "    v = offset - v;",
+      "  }",
+      "  gl_FragColor = vec4(v, v, v, 1.0);",
+      "}"
+    ].join("\n")
+
+    log("vertex shader setup")
+    var vertexShader = setupShader(vertexSource, glCtx.VERTEX_SHADER)
+    log("fragment shader setup")
+    var fragmentShader = setupShader(fragmentSource, glCtx.FRAGMENT_SHADER)
+    if (vertexShader === null || fragmentShader === null) {
+      log("shader setup failed")
+      return
+    }
+
+    log("program setup")
+    var program = setupProgram(vertexShader, fragmentShader)
+    if (vertexShader === null || fragmentShader === null) {
+      log("program setup failed")
+      return
+    }
+    glCtx.useProgram(program)
+
+    var vertexPosition = glCtx.getAttribLocation(program, 'aVertexPosition')
+    glCtx.enableVertexAttribArray(vertexPosition)
+    var vertexTexCoord = glCtx.getAttribLocation(program, 'aStrobeCoord')
+    glCtx.enableVertexAttribArray(vertexTexCoord)
+
+    var gainUniform = glCtx.getUniformLocation(program, 'gain')
+    var offsetUniform = glCtx.getUniformLocation(program, 'offset')
+
+    var vertexBuf = glCtx.createBuffer()
+    var vertexCoordBuf = glCtx.createBuffer()
+    var vertexIndexBuf = glCtx.createBuffer()
+    inited = true
+
     draw = function() {
+      if (!inited) {
+        return
+      }
+      if (me.bufferLen <= 0) {
+        return
+      }
+      // glCtx.enableVertexAttribArray(vertexTexCoord)
+
+      var vertexData = new Float32Array(4*(2+3)*me.bufferLen)
+      var vertexIdxs = new Uint16Array(2*3*me.bufferLen)
+
+      for (var i = 0; i < me.bufferLen; i++) {
+        var val = me.buffer[i] + me.offset
+        vertexData[(2+3)*(4*i+0) + 0] = 1.0
+        vertexData[(2+3)*(4*i+0) + 1] = 2.0
+        vertexData[(2+3)*(4*i+0) + 2] = 1.0
+        vertexData[(2+3)*(4*i+0) + 3] = 1.0
+        vertexData[(2+3)*(4*i+0) + 4] = val
+
+        vertexData[(2+3)*(4*i+1) + 0] = 1.0
+        vertexData[(2+3)*(4*i+1) + 1] = -2.0
+        vertexData[(2+3)*(4*i+1) + 2] = 1.0
+        vertexData[(2+3)*(4*i+1) + 3] = -1.0
+        vertexData[(2+3)*(4*i+1) + 4] = val
+
+        vertexData[(2+3)*(4*i+2) + 0] = -1.0
+        vertexData[(2+3)*(4*i+2) + 1] = -2.0
+        vertexData[(2+3)*(4*i+2) + 2] = 0.0
+        vertexData[(2+3)*(4*i+2) + 3] = -1.0
+        vertexData[(2+3)*(4*i+2) + 4] = val
+
+        vertexData[(2+3)*(4*i+3) + 0] = -1.0
+        vertexData[(2+3)*(4*i+3) + 1] = 2.0
+        vertexData[(2+3)*(4*i+3) + 2] = 0.0
+        vertexData[(2+3)*(4*i+3) + 3] = 1.0
+        vertexData[(2+3)*(4*i+3) + 4] = val
+      }
+      for (var i = 0; i < me.bufferLen; i++) {
+        vertexIdxs[2*3*i + 0] = 4*i + 0;
+        vertexIdxs[2*3*i + 1] = 4*i + 1;
+        vertexIdxs[2*3*i + 2] = 4*i + 2;
+        vertexIdxs[2*3*i + 3] = 4*i + 0;
+        vertexIdxs[2*3*i + 4] = 4*i + 2;
+        vertexIdxs[2*3*i + 5] = 4*i + 3;
+      }
+
+      log(me.bufferLen)
+      log(vertexData.slice(0, 6))
+      glCtx.bindBuffer(glCtx.ARRAY_BUFFER, vertexBuf)
+      glCtx.bufferData(glCtx.ARRAY_BUFFER, vertexData, glCtx.DYNAMIC_DRAW)
+
+      glCtx.clear(glCtx.COLOR_BUFFER_BIT)
+
+      glCtx.vertexAttribPointer(vertexPosition, 2, glCtx.FLOAT, false, 5*4, 0)
+      glCtx.vertexAttribPointer(vertexTexCoord, 3, glCtx.FLOAT, false, 5*4, 2*4)
+
+      glCtx.bindBuffer(glCtx.ELEMENT_ARRAY_BUFFER, vertexIndexBuf)
+      glCtx.bufferData(glCtx.ELEMENT_ARRAY_BUFFER, vertexIdxs, glCtx.DYNAMIC_DRAW)
+
+      glCtx.uniform1f(gainUniform, me.brightGain)
+      glCtx.uniform1f(offsetUniform, me.brightOffset)
+
+      glCtx.drawElements(glCtx.TRIANGLES, 2*me.bufferLen, glCtx.UNSIGNED_SHORT, 0)
+      glCtx.finish()
+    }
+
+    clearGlState = function() {
+      // TODO
     }
   }
 
@@ -86,12 +252,18 @@ function Strobe(audioCtx, glCtx) {
   }
 
   // used to set the mode
-  this.useOnePoly = initOnePoly
+  this.resetGlState = function() {
+    if (clearGlState !== null) {
+      clearGlState()
+    }
+    clearGlState = null
+    draw = null
+  }
   this.useManyPoly = initManyPoly
 
   // init WebGL context with the default shaders
-  this.useOnePoly()
+  this.useManyPoly()
 }
 // about the number of values received in 1/30th of a second
-Strobe.BUF_SZ = 2048
-Strobe.MAX_BUF_SZ = 65536
+StrobeTuner.BUF_SZ = 2048
+StrobeTuner.MAX_BUF_SZ = 16384
